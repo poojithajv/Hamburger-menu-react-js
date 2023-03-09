f0ebe864fc9bf81a5beb86a06c2267c0635488ce
poojithajv <poojithajv15@gmail.com>
2023-03-09 10:12:39 +0000

    hamburger icon react js

---
+++ src/components/Header/index.js
@@ -0,0 +1,64 @@
+// Write your code here
+import Popup from 'reactjs-popup'
+import {Link} from 'react-router-dom'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {IoMdClose} from 'react-icons/io'
+import {AiFillHome} from 'react-icons/ai'
+import {BsInfoCircleFill} from 'react-icons/bs'
+
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
+      <Link to="/">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+          alt="website logo"
+          className="website-logo"
+        />
+      </Link>
+      <Popup
+        modal
+        trigger={
+          <button
+            type="button"
+            className="hamburger-icon-button"
+            data-testid="hamburgerIconButton"
+          >
+            <GiHamburgerMenu size="30" />
+          </button>
+        }
+        className="popup-content"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              className="close-button"
+              type="button"
+              onClick={() => close()}
+              data-testid="closeButton"
+            >
+              <IoMdClose size="30" />
+            </button>
+            <ul className="nav-links-list">
+              <li className="nav-link-item">
+                <Link className="nav-link" to="/" onClick={() => close()}>
+                  <AiFillHome size="35" />
+                  <p className="nav-link-content">Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/about" onClick={() => close()}>
+                  <BsInfoCircleFill size="30" />
+                  <p className="nav-link-content">About</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </Popup>
+    </div>
+  </nav>
+)
+export default Header
